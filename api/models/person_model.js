9429233e87a8235b68a76a2c55405ced2e5d8cd5
NLAIV <juho.nuorala@gmail.com>
2021-03-23 09:42:53 +0200

    person api tehty

---
+++ api/models/person_model.js
@@ -0,0 +1,28 @@
+const db = require('../database');
+
+const person = {
+  getById: function(id, callback) {
+    return db.query('select * from person where id_person=?', [id], callback);
+  },
+  getAll: function(callback) {
+    return db.query('select * from person', callback);
+  },
+  add: function(person, callback) {
+    return db.query(
+      'insert into person (fname,lname,balance) values(?,?,?)',
+      [person.fname, person.lname, person.balance],
+      callback
+    );
+  },
+  delete: function(id, callback) {
+    return db.query('delete from person where id_person=?', [id], callback);
+  },
+  update: function(id, person, callback) {
+    return db.query(
+      'update person set fname=?,lname=?, balance=? where id_person=?',
+      [person.fname, person.lname, person.balance, id],
+      callback
+    );
+  }
+};
+module.exports = person;
